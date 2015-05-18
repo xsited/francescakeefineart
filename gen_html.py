752f18db7b36a6d94475a57b327afe594ace3a75
Pavel V <src@yekm.name>
2015-05-18 23:50:08 +0300

    option to use original FAA links to full page

--- gen_html.py
+++ gen_html.py
@@ -1,8 +1,13 @@
 #!/usr/bin/python
 
-import sqlite3, re, os
+import sqlite3, re, os, argparse
 from jinja2 import Template
 
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--faa",
+        help="Use original FAA links",
+        action="store_true")
+args = parser.parse_args()
 # http://stackoverflow.com/questions/3300464/how-can-i-get-dict-from-sqlite-query
 def dict_factory(cursor, row):
     d = {}
@@ -42,13 +47,15 @@ for row in rows:
         where misc.p_id = ?
         """, [ row['rowid'] ])
     info = c.fetchone()
-    print info
 
-    row['painting_html'] = re.sub('[^a-zA-Z\d]', '_', row['title'].lower()) + '.html'
-    print "generating " + row['painting_html']
-    open('site' + os.sep + row['painting_html'], 'w+').write(
-            painting_template.render(painting=row, awards=awards, info=info)
-            )
+    if args.faa:
+        row['painting_html'] = row['full_url']
+    else:
+        row['painting_html'] = re.sub('[^a-zA-Z\d]', '_', row['title'].lower()) + '.html'
+        print "generating " + row['painting_html']
+        open(os.path.join('site', row['painting_html']), 'w+').write(
+                painting_template.render(painting=row, awards=awards, info=info)
+                )
 
-open('site' + os.sep + 'index.html', 'w+').write(index_template.render(paintings=rows))
+open(os.path.join('site', 'index.html'), 'w+').write(index_template.render(paintings=rows))
 
